c75ce2589d30ab695b825fb4bfb52c0f89547e4d
jaejun <rhwowns1@naver.com>
2018-07-08 19:40:39 +0900

    관리자 재수정

--- semiProject/src/com/what/semi/common/template/PageTemplate.java
+++ semiProject/src/com/what/semi/common/template/PageTemplate.java
@@ -1,12 +1,12 @@
 package com.what.semi.common.template;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.what.semi.blackList.model.service.BlackListService;
 import com.what.semi.blackList.model.vo.ConditionVo;
 import com.what.semi.contract.model.service.ContractService;
+import com.what.semi.managePost.model.service.ManagePostService;
+import com.what.semi.managePost.model.vo.PostCondition;
 import com.what.semi.qna.model.service.QnaService;
 import com.what.semi.qna.model.vo.QnaVo;
 import com.what.semi.recruitment.model.service.RecruitmentService;
@@ -329,5 +329,37 @@ public class PageTemplate {
 				
 				PageInfo pi = new PageInfo(currentPage, limit, maxPage, startPage, endPage, listCount);		
 				return pi;
+	}
+	
+	public static PageInfo postPaging(HttpServletRequest request, ManagePostService mps, PostCondition pc) {
+		//페이징 처리 변수
+	      int currentPage = 1;   //현재 페이지의 번호(default=1)
+	      int limitPage = 10;      //한 페이지에 출력할 페이지 개수
+	      int maxPage;         //가장 마지막 페이지
+	      int startPage;         //시작 페이지
+	      int endPage;         //마지막 페이지 변수
+	      int limit = 10;         //한 페이지에 출력할 글의 개수(default=12)
+	      int listCount;
+	            
+	      //게시글의 총 갯수
+	      listCount = mps.managePostCount(pc);
+	            
+	      if(null != request.getParameter("currentPage") && !"".equals(request.getParameter("currentPage"))) {                     //페이지가 이동되었을경우
+	         currentPage = Integer.parseInt(request.getParameter("currentPage"));   //넘어온 페이지 값을 현재페이지로 설정
+	      }
+	      
+	      //130 -> 13
+	      maxPage = (int)((double)listCount / limit + 0.9);
+	      //현재 페이지 번호
+	      //12 - 10
+	      startPage = (int)(currentPage / limitPage * limitPage) + 1;
+	      //11~20 -> 134 -> 14
+	      endPage = startPage + limitPage - 1;
+	      if(maxPage < endPage) {
+	         endPage = maxPage;
+	      }
+	            
+	      PageInfo pi = new PageInfo(currentPage, limit, maxPage, startPage, endPage, listCount);      
+	      return pi;
 	}
 }
